81e5ed3d0e276609a018330eeb51acb9b59c447a
Thomas Schoemaecker <tdub@schoem.org>
2024-01-29 18:21:50 +0000

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(express.json())
-app.use(requestLogger)
-app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { 

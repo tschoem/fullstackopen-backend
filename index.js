22b3b9a57c43f9aa702ea4f4c345407222049b19
Thomas Schoemaecker <tdub@schoem.org>
2024-02-05 21:10:23 +0000

    3.15: Phonebook database, step 3

--- index.js
+++ index.js
@@ -19,24 +19,24 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
-  { 
+  {
     "id": 1,
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": 2,
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": 3,
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": 4,
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ]
@@ -54,7 +54,7 @@ app.get('/', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
-  
+
   if (person) {
     response.json(person)
   } else {
@@ -64,20 +64,27 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(pers => pers.id === id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.json(person)
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      if (result) {
+        response.json(result)
+       } else {
+        response.status(204).end()
+       }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const generateId = () => {
   var randomID
-  
+
   do {
     randomID = Math.floor(Math.random() * 1000000000000)
   } while (persons.find(person => person.id === randomID))
-  
+
   return randomID
 }
 
@@ -85,20 +92,20 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'Name missing' 
+    return response.status(400).json({
+      error: 'Name missing'
     })
   }
-  
+
   if (!body.number) {
-    return response.status(400).json({ 
-      error: 'Number missing' 
+    return response.status(400).json({
+      error: 'Number missing'
     })
   }
 
   if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'Name must be unique' 
+    return response.status(400).json({
+      error: 'Name must be unique'
     })
   }
 
